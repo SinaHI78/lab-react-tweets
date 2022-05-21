95ac63a58e6367804b54a0e37cebfdb1ae960438
Sina Joos <sinajoos@gmx.de>
2022-05-21 12:00:11 +0200

    iterations 1-4 done

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,11 @@
+import App from '../App';
+
+function ProfileImage(props) {
+  return (
+    <div className="profileImage">
+      <img src={props.image} className="profile" alt="profile" />
+    </div>
+  );
+}
+
+export default ProfileImage;
